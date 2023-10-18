52e3b4e07c49e3be9d5a93234c35d150655faa3c
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-10-18 10:33:21 -0400

    feat: criando relação manytoone entre restaurante e cozinha

--- src/main/java/com/nelcfood/model/entities/Restaurante.java
+++ src/main/java/com/nelcfood/model/entities/Restaurante.java
@@ -23,4 +23,7 @@ public class Restaurante {
     @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
 
+    @ManyToOne
+    private Cozinha cozinha;
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
-insert into cozinha(nome) values ('Tailandesa');
-insert into cozinha(nome) values ('Indiana');
+insert into cozinha(id, nome) values (1, 'Tailandesa');
+insert into cozinha(id, nome) values (2, 'Indiana');
+
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Marmitaria Sinhá', 0, 1);
+insert into restaurante(nome, taxa_frete, cozinha_id) values ('Zebu', 10, 2);
