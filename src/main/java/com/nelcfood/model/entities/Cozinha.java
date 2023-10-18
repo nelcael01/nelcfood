436d3146dac5391e558e05e871fdedbe977dbedb
nelcael07 <47829164+nelcael07@users.noreply.github.com>
2023-10-18 10:06:51 -0400

    feat: criando repository de restaurante + adição da lib lombok

--- src/main/java/com/nelcfood/model/entities/Cozinha.java
+++ src/main/java/com/nelcfood/model/entities/Cozinha.java
@@ -1,42 +1,21 @@
 package com.nelcfood.model.entities;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cozinha {
+
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String nome;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(id, cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
---
+++ src/main/java/com/nelcfood/model/repository/RestauranteRepository.java
@@ -0,0 +1,8 @@
+package com.nelcfood.model.repository;
+
+import com.nelcfood.model.entities.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+}
