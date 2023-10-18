a1afcf68815e28cb617b612de6bf02bb2c5b5d1c
nelcael07 <alvesnelcael@gmail.com>
2023-10-17 21:21:28 -0400

    feat: criando projeto spring 2.1.7 java 11

---
+++ src/main/java/com/nelcfood/nelcfood/NelcfoodApplication.java
@@ -0,0 +1,13 @@
+package com.nelcfood.nelcfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelcfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelcfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelcfood/nelcfood/NelcfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelcfood.nelcfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelcfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
