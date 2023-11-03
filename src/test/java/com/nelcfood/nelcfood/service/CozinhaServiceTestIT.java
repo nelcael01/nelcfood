fed2cb15b2692fa3b28b662e89b482354e8c04a9
nelcael07 <alvesnelcael@gmail.com>
2023-11-02 21:53:37 -0400

    feat: criando test para chamada listar de cozinhas

--- src/test/java/com/nelcfood/nelcfood/service/CozinhaServiceTestIT.java
+++ src/test/java/com/nelcfood/nelcfood/service/CozinhaServiceTestIT.java
@@ -1,13 +1,30 @@
 package com.nelcfood.nelcfood.service;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CozinhaServiceTestIT {
-  public void deveRetonar200QuandoListarCozinhas() {
 
+  @LocalServerPort
+  private int port;
+
+  @Test
+  public void deveRetonar200QuandoListarCozinhas() {
+    RestAssured
+            .given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+            .when()
+            .get()
+            .then()
+            .statusCode(200);
   }
 }
