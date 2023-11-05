36176f1590b4edb0dcad41dd01df94f165546dfb
nelcael07 <alvesnelcael@gmail.com>
2023-11-05 13:58:52 -0400

    feat: criando teste de api deveRetornar201QuandoCadastrarCozinha

--- src/test/java/com/nelcfood/nelcfood/service/CozinhaServiceTestIT.java
+++ src/test/java/com/nelcfood/nelcfood/service/CozinhaServiceTestIT.java
@@ -2,10 +2,14 @@ package com.nelcfood.nelcfood.service;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,16 +19,37 @@ public class CozinhaServiceTestIT {
   @LocalServerPort
   private int port;
 
+  @Autowired
+  private Flyway flyway;
+
+  @Before
+  public void setUp() {
+    RestAssured.basePath = "/cozinhas";
+    RestAssured.port = port;
+    flyway.migrate();
+  }
+
   @Test
   public void deveRetonar200QuandoListarCozinhas() {
     RestAssured
             .given()
-            .basePath("/cozinhas")
-            .port(port)
             .accept(ContentType.JSON)
             .when()
             .get()
             .then()
-            .statusCode(200);
+            .statusCode(HttpStatus.OK.value());
+  }
+
+  @Test
+  public void deveRetornar201QuandoCadastrarCozinha() {
+    RestAssured
+            .given()
+            .body("{ \"nome\": \"Chinesa\" }")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .when()
+            .post()
+            .then()
+            .statusCode(HttpStatus.CREATED.value());
   }
 }
